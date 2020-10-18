75b513510da158799095f1416818f9101e5972e6
elecdeer <elecdeer@users.noreply.github.com>
2020-10-18 09:32:52 +0000

    Automated deployment: Sun Oct 18 09:32:52 UTC 2020 d4fa409dcd94e8630bd51dd0325b7c897fdd2126

---
+++ precache-manifest.534fad87fdfce33cd693ae3e7ebcf781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836c55a65db1ca61bfe517ef49d28947",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a5cb4fb8c2c53859447",
+    "url": "./static/css/main.d9c16d08.chunk.css"
+  },
+  {
+    "revision": "aacc8d0241fa2341fa06",
+    "url": "./static/js/2.ad0a13e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ad0a13e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a5cb4fb8c2c53859447",
+    "url": "./static/js/main.286b9ccb.chunk.js"
+  },
+  {
+    "revision": "80fea88e9ab19ee008b8",
+    "url": "./static/js/runtime-main.375dcc30.js"
+  }
+]);
